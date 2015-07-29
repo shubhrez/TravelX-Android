fb39ee57e4a72c66e5005185a70bec7b602ee52f
Shubham Goyal <shubhamdrolia87@gmail.com>
2015-07-29 11:29:59 +0530

    first commit

---
+++ app/src/main/java/com/travelx/MainHomeActivity.java
@@ -0,0 +1,13 @@
+package com.travelx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainHomeActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_home_layout);
+    }
+}
---
+++ app/src/main/java/com/travelx/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.travelx;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends Activity {
+
+    Intent intent = null;
+    CountDownTimer countDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_layout);
+
+        intent = new Intent(this,MainHomeActivity.class);
+        countDownTimer = new CountDownTimer(1500, 500) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+            @Override
+            public void onFinish() {
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        countDownTimer.start();
+    }
+
+}
